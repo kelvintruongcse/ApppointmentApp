c1e688d4668f73542e3dd91522e8d007c65f8fce
binhtp <kelvintruongcse@gmail.com>
2026-05-26 14:17:06 +0700

    Remove unused imports in SplashScreen.kt
    * Clean up `SplashScreen.kt` by removing unused `TextUnit` and `org.w3c.dom.Text` imports.

--- app/src/main/java/kelvintruongcse/github/apppointmentapp/ui/theme/SplashScreen.kt
+++ app/src/main/java/kelvintruongcse/github/apppointmentapp/ui/theme/SplashScreen.kt
@@ -25,11 +25,9 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kelvintruongcse.github.apppointmentapp.R
-import org.w3c.dom.Text
 
 
 @Composable
